48499ced0ec4f587d81350ee5eb82a188a8438e0
JarekParal <paral.jarek@gmail.com>
2019-08-19 14:18:19 +0200

    Change name of all files to name of plugin - Domecek

--- Domecek/domecek.php
+++ Domecek/domecek.php
@@ -10,18 +10,18 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Domecek
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin Name:       Domecek WordPress Plugin
  * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Domecek - WordPress Plugin for information system IS Domecek.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Jaroslav Páral
+ * Author URI:        https://github.com/JarekParal/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       Domecek
  * Domain Path:       /languages
  */
 
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'DOMECEK_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
